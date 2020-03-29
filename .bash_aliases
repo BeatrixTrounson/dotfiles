4f5a37ee2b4455a8185c9809a9da168004e0362c
Benjamin Trounson <btrounson@outlook.co.nz>
2020-03-30 01:43:55 +1300

    dotfile refresh

--- .bash_aliases
+++
@@ -1,3 +0,0 @@
-alias vim='nvim'
-alias ls='ls -x -l -o -g'
-alias yta='youtube-dl --yes-playlist --embed-thumbnail --metadata-from-title "%(artist)s - %(title)s" --audio-format mp3 -x'
--- .dwm/autostart.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-wal -q -i /home/beatrix/Pictures/Wallpaper-2.png &
-
-while true; do
-	xsetroot -name "` date +"%b %e, %Y - %R" `"
-	sleep 1m 	# Update time every minute
-done &
